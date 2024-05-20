7ef842bca59f7b187f557f4f44bc6c9cc546326b
Juan Antonio Molina Rey <JuanMolinaRey7@gmail.com>
2024-05-20 15:14:08 +0200

    *

--- Bucles/2.sh
+++ Bucles/2.sh
@@ -1,17 +1,22 @@
 #!/bin/Bash
 
+borrar="No valida"
+
 read -p "Agregar un nombre de archivo: " respuesta
 touch "$respuesta"
 Echo "Has creado el archivo $respuesta"
 sleep 3
 ls -l
 
+while [ "$borrar" != "Y" ]; do
 read -p "¿Quieres borrar algún archivo de la lista?: (Y/N) " borrar
-if [ $borrar = "Y" ]; then
+if [ "$borrar" = "Y" ]; then
 read -p "Escriba el nombre: " name
 read -p "Escriba el formato: " formato
 rm $name$formato
-
-else [ $borrar = "N" ];
+elif [ "$borrar" = "N" ]; then
 echo "Ok, no borraremos ningún archivo"
+else 
+echo "$borrar No es una respuesta valida"
 fi
+done
